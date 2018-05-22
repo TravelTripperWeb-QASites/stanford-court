6725fe331a8892d7d93fe92e03b73c9ce8e334fe
Hari Kumar <hracha@traveltripper.com>
2018-05-22 13:52:13 +0530

    on the header

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -1,3 +1,19 @@
 /*
    jQuery plugin settings and other scripts
    ========================================================================== */
+
+winResize(); //from function.js
+navOnScroll(); //from function.js
+
+$(window).on("load", function () {
+   // nav menu on hover
+   $('ul.navbar-nav li.dropdown a').attr('data-toggle', 'disable');
+     if ($(window).width() > 991) {
+       //Add Hover effect to menus
+       jQuery('ul.navbar-nav li.dropdown').hover(function() {
+           jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn();
+       }, function() {
+           jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut();
+       });
+   }
+});
