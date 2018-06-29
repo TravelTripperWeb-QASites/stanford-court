818568b6479554c527ae5e6f67b5c69537b6add0
Kiran <kmekala@traveltripper.com>
2018-06-29 16:02:57 +0530

    Terms of Use checkbox implementation as per GDPR guidlines for all forms

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -401,3 +401,57 @@ $(function() {
   		$(".price-wrap, .recentBookingsInner").css('display', 'block');
   	}, 2000);
 });
+
+
+
+// Restrict form submission by disabling submit button until all required fields are filled (W.r.t GDPR Guidelines)
+$(document).ready(function () {
+function checkForm() {
+  // here, "this" is an input element
+  var isValidForm = true;
+  $(this.form)
+    .find(':input[required]:visible')
+    .each(function() {
+      if (!this.value.trim()) {
+        isValidForm = false;
+      }
+    });
+  $(this.form)
+    .find('input[type="checkbox"]:required')
+    .each(function() {
+      if (!$(this)
+        .is(':checked')) {
+        isValidForm = false;
+      }
+    });
+  $(this.form)
+    .find('select:required')
+    .each(function() {
+      if (!$(this)
+        .find('option:selected')
+        .val()
+        .trim()) {
+        isValidForm = false;
+      }
+    });
+
+  $(this.form)
+    .find('#subscribe, #business-btn, #contact-btn') // Button class names should be unique for every form
+    .prop('disabled', !isValidForm);
+  return isValidForm;
+}
+
+$('#subscribe, #business-btn, #contact-btn') // Button class names should be unique for every form
+  .closest('form')
+  // indirectly bind the handler to form
+  .submit(function() {
+    return checkForm.apply($(this)
+      .find(':input')[0]);
+  })
+  // look for input elements
+  .find(':input[required]:visible')
+  // bind the handler to input elements
+  .on('change keyup', checkForm)
+  // immediately fire it to initialize buttons state
+  .keyup();
+});
