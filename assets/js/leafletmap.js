28aa0bd1fc749151b4f37425aa2357b9b93436b2
Renu <rgunpal@traveltripper.com>
2018-08-16 23:38:37 +0530

    Add map interactions

--- assets/js/leafletmap.js
+++ assets/js/leafletmap.js
@@ -1,35 +1,35 @@
-/**
- * LEAFLET CUSTOM MAP PLUGIN
- **/
+// Leaflet Interactive Map Plugin
 
 (function ($) {
   $.fn.leafMap = function (options) {
     var self     = this;
     var defaults = {
-        hotelTitle      : 'Bedrock',
-        hotelAddress    : '9 Crosby St New York, NY 10013',
-        hotelLat        : 40.719892,
-        hotelLong       : -74.000173,
-        hotelMarker     : '/assets/images/map-marker.png',
-        markerSize      : [36, 46],
-        zoom            : 14,
-        minZoom         : 0,
-        maxZoom         : 18,
-        fitBounds       : true,
-        attrIconClass   : 'map-circle-icon',
-        attrLabel       : true,
-        markerLabelText : true,
-        categorytypeIcon: false,
-        scrollWheelZoom : false,
-        scrollOnClick   : true,
-        getDirectionBtn : false,
-        googleLink      : false,
-        websiteLink     : false,
-        getDirectionBtnLabel: 'Get Direction',
-        hideMarkerLabelHover: true,
-        TileStyle       : 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png',
-        attribution     : 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-      },
+      hotelTitle      : 'Bedrock',
+      hotelAddress    : '9 Crosby St New York, NY 10013',
+      hotelLat        : 40.719892,
+      hotelLong       : -74.000173,
+      hotelMarker     : '/assets/images/map-marker.png',
+      markerSize      : [36, 46],
+      zoom            : 14,
+      minZoom         : 0,
+      maxZoom         : 20,
+      fitBounds       : true,
+      attrIconClass   : 'map-circle-icon',
+      attrLabel       : false,
+      showPopup       : true,
+      markerLabelText : false,
+      categorytypeIcon: false,
+      scrollWheelZoom : false,
+      scrollOnClick   : true,
+      getDirectionBtn : false,
+      googleLink      : false,
+      websiteLink     : false,
+      getDirectionBtnLabel  : 'Get Direction',
+      hideMarkerLabelHover  : true,
+      markerClickAction     : false,
+      TileStyle             : 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png',
+      attribution           : 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+    },
 
     options                       = $.extend(defaults, options);
     var mapID                     = self.attr('id'); //map element id
@@ -37,7 +37,7 @@
     var formattedAttractionsList  = [];
     var attractionsfilter         = {};
 
-    //set hotel marker icon
+    // Set hotel marker icon
     var hotelIcon = L.icon({
       iconUrl: options.hotelMarker,
       iconSize: options.markerSize
@@ -54,10 +54,10 @@
         maxZoom    : options.maxZoom
       }).addTo(map);
 
-      // enable scroll on map after clicking on it.
+      // Enable scrolling on map after clicking
       map.on('click', function () {
         if(!options.scrollWheelZoom && options.scrollOnClick) {
-          if (map.scrollWheelZoom.enabled()) {// if scroll is already enabled then click on it should disable the scroll.
+          if (map.scrollWheelZoom.enabled()) { // if scroll is already enabled then clicking should disable it
             map.scrollWheelZoom.disable();
           } else {
             map.scrollWheelZoom.enable();
@@ -65,7 +65,7 @@
         }
       });
 
-      //hotel's marker
+      // Set hotel marker
       var marker = L.marker([options.hotelLat, options.hotelLong], {
         title: options.hotelTitle,
         alt  : 'Hotel Map Marker',
@@ -79,7 +79,7 @@
       }
     }
 
-    //set attractions markers
+    // Set attraction markers
     function attractionMarkersIcon($value, $category) {
       var attractionIcon;
       var iconObj = {
@@ -96,36 +96,44 @@
     }
 
     function loadAttractionMarkers() {
-      //set attraction marker
       for (var i = 0; i < attractionsArray.length; i++) {
         var infoText ='';
-        if (options.googleLink) {
-          infoText = '<a href="http://maps.google.com/maps?q='+attractionsArray[i][0]+'+'+ attractionsArray[i][4]+'" target="_blank"><h4>' + attractionsArray[i][0] + '</h4>' + attractionsArray[i][4]+ '</a>';
-        }else if(!options.websiteLink && options.getDirectionBtn){
-          infoText = '<h4>' + attractionsArray[i][0] + '</h4>' + attractionsArray[i][4]+ ' <br><a class="btnDirection" href="http://maps.google.com/maps?q='+ encodeURIComponent(attractionsArray[i][0]).replace(/ /g,'+')+'+'+ encodeURIComponent(attractionsArray[i][4]).replace(/ /g,'+')+'" target="_blank">'+options.getDirectionBtnLabel+'</a>';
-        }else if( options.websiteLink && !options.getDirectionBtn) {
-          infoText = '<h4><a href="'+attractionsArray[i][5]+'">' + attractionsArray[i][0] + '</a></h4>' + attractionsArray[i][4];
-        }
-        else if( options.websiteLink && options.getDirectionBtn) {
-        infoText = '<h4><a href="'+attractionsArray[i][5]+'">' + attractionsArray[i][0] + '</a></h4>' + attractionsArray[i][4]+ ' <br><a class="btnDirection" href="http://maps.google.com/maps?q='+encodeURIComponent(attractionsArray[i][0]).replace(/ /g,"+")+'+'+ encodeURIComponent(attractionsArray[i][4]).replace(/ /g,'+')+'" target="_blank">'+options.getDirectionBtnLabel+'</a>';
-        }
-        else{
-          infoText = '<h4>' + attractionsArray[i][0] + '</h4>' + attractionsArray[i][4];
-        }
 
-        var category                = attractionsArray[i][3];
+
+        var category  = attractionsArray[i][3];
         var iconClass = options.categorytypeIcon ? attractionsArray[i][3] : '';
         var iconLabel = options.markerLabelText ? attractionsArray[i][0] : i + 1;
 
         attractionsfilter[category] = attractionsfilter[category] || [];
 
         marker = new L.marker([attractionsArray[i][1], attractionsArray[i][2]], {
-          title: options.hideMarkerLabelHover ? '': attractionsArray[i][0],
-          alt  : attractionsArray[i][3],
-          icon : attractionMarkersIcon(iconLabel, iconClass),
-          riseOnHover: true
-        }).bindPopup(infoText).addTo(map);
+          title       : options.hideMarkerLabelHover ? '' : attractionsArray[i][0],
+          alt         : attractionsArray[i][3],
+          marker_item : i+1,
+          icon        : attractionMarkersIcon(iconLabel, iconClass),
+          riseOnHover : true
+        });
 
+        if(options.showPopup) {
+          if (options.googleLink) {
+            infoText = '<a href="http://maps.google.com/maps?q='+attractionsArray[i][0]+'+'+ attractionsArray[i][4]+'" target="_blank"><h4>' + attractionsArray[i][0] + '</h4>' + attractionsArray[i][4]+ '</a>';
+          }else if(!options.websiteLink && options.getDirectionBtn) {
+            infoText = '<h4>' + attractionsArray[i][0] + '</h4>' + attractionsArray[i][4]+ ' <br><a class="btnDirection" href="http://maps.google.com/maps?q='+ encodeURIComponent(attractionsArray[i][0]).replace(/ /g,'+')+'+'+ encodeURIComponent(attractionsArray[i][4]).replace(/ /g,'+')+'" target="_blank">'+options.getDirectionBtnLabel+'</a>';
+          }else if( options.websiteLink && !options.getDirectionBtn) {
+            infoText = '<h4><a href="'+attractionsArray[i][5]+'">' + attractionsArray[i][0] + '</a></h4>' + attractionsArray[i][4];
+          }
+          else if( options.websiteLink && options.getDirectionBtn) {
+          infoText = '<h4><a href="'+attractionsArray[i][5]+'">' + attractionsArray[i][0] + '</a></h4>' + attractionsArray[i][4]+ ' <br><a class="btnDirection" href="http://maps.google.com/maps?q='+encodeURIComponent(attractionsArray[i][0]).replace(/ /g,"+")+'+'+ encodeURIComponent(attractionsArray[i][4]).replace(/ /g,'+')+'" target="_blank">'+options.getDirectionBtnLabel+'</a>';
+          }
+          else {
+            infoText = '<h4>' + attractionsArray[i][0] + '</h4>' + attractionsArray[i][4];
+          }
+          marker = marker.bindPopup(infoText);
+        }
+
+        marker.addTo(map);
+
+        marker.on('click', markerClickDefaultAction);
         var attractionObj = {
           lat     : attractionsArray[i][1],
           lon     : attractionsArray[i][2],
@@ -139,36 +147,47 @@
       }
     }
 
+    function markerClickDefaultAction(e) {
+      var mlat = e.target._latlng.lat;
+      var mlng = e.target._latlng.lng;
+      map.panTo({
+        lat: mlat,
+        lng: mlng
+      });
+      if(options.markerClickAction && options.markerClickAction instanceof Function) {
+        options.markerClickAction(e);
+      }
+    }
+
     function setCategoriesFilter() {
       var categories             = Object.keys(attractionsfilter);
       var mapcategoryFilterEle   = $("[data-mapcategory-filter]");
 
-      // add filter html
+      // Add filter
       if (categories.length > 1) {
-        //Set up tabs
+        // Setup tabs
         if (mapcategoryFilterEle) {
-          // first add 'All' option
+          // First add 'All' option
           mapcategoryFilterEle.append(
             "<li class=\"nav-item\"><a class=\"nav-link active\" data-category='all'>All</a></li>"
           );
-          // now add all categories html
+          // Now add all categories
           for (var i = 0, ii = categories.length; i < ii; i++) {
             mapcategoryFilterEle.append(
               "<li class=\"nav-item " + $.trim(categories[i]).toLowerCase() + "\"><a class=\"nav-link\" data-category=\"" + categories[i] + "\">" + categories[i] + "</a></li>"
             );
           }
         }
-
       }
 
-      // define category filter behaviour
+      // Define category filter behaviour
 
       $("[data-category]").click(function () {
-        var bound = []; //to set auto fit bound
+        var bound = []; // Set autofit bound
         var selectedCategory = $(this).data('category');
         $(this).parent('li').siblings().find('a').removeClass('active');
         $(this).addClass('active');
-        // show hide attractions from html list
+        // Show/hide attractions from html list
         if (selectedCategory != "all") {
           $('[data-mappable-category]').hide();
           $('[data-mappable-category="' + selectedCategory + '"]').show();
@@ -176,7 +195,7 @@
           $('[data-mappable-category]').show();
         }
 
-        // show hide markers based on selected attractions category or 'All'
+        // Show/hide markers based on selected attractions category or 'All'
         for (var i = 0; i < formattedAttractionsList.length; i++) {
           if (formattedAttractionsList[i].category == selectedCategory || selectedCategory == "all") {
             formattedAttractionsList[i].marker.addTo(map);
@@ -186,7 +205,7 @@
           }
         }
 
-        //category based fit bounds map
+        // Category based fit bounds map
         setTimeout(function(){
           if (options.fitBounds) {
             map.fitBounds(bound);
@@ -199,9 +218,9 @@
       var mapcategoryFilterEle   = $("[data-mapcategory-filter]");
       var mapCategoryHtmlListEle = $("[data-mapcategory-list]");
 
-      //show all list of attractions
+      // Show all attractions
       if (mapCategoryHtmlListEle) {
-        // add attractions list items
+        // Add attractions items
         $.each(attractionsfilter, function (key1, value1) {
           mapCategoryHtmlListEle.append('<div data-mappable-category="' + key1 + '"><h3>' + key1 + '</h3><ul class="attraction-lists list-' + key1 + '"></ul></div>');
           for (var i = 0; i < value1.length; i++) {
@@ -209,34 +228,48 @@
           };
         });
 
-        // define each attractions 'on hover' and click behaviour
+        // Define each attractions 'on hover' and 'on click' behaviour
         $("[data-mappable-item]").each(function () {
           var categoryitem = $(this).data('mappable-category');
           var itemID       = $(this).data('mappable-item');
 
-          // on hover highlight selected attraction's marker
+          // 'On hover' highlight selected attraction's marker
           $(this).hover(function () {
-            $(marker.icon).removeClass('active');
             $(attractionsfilter[categoryitem][itemID].marker._icon).addClass('active');
           }, function () {
             $(attractionsfilter[categoryitem][itemID].marker._icon).removeClass('active');
           });
 
-          // on click show selected attraction's  infobox
-          $(this).click(function () {
-            var mlat = $(attractionsfilter[categoryitem][itemID].marker._latlng.lat);
-            var mlng = $(attractionsfilter[categoryitem][itemID].marker._latlng.lng);
-            mlat.push.apply(mlat, mlng);
-
-            map.panTo({
-              lat: mlat[0],
-              lng: mlat[1]
+          // 'On click' show selected attraction's popup if enabled and show active Category name in filter tab
+          $(this).click(function (event) {
+            $.each(attractionsfilter[categoryitem], function(i, item){
+                $(item.marker._icon).removeClass('active');
             });
-            attractionsfilter[categoryitem][itemID].marker.openPopup();
+
+            setTimeout(function(){
+              if (options.fitBounds) {
+                map.fitBounds([$(attractionsfilter[categoryitem][itemID].marker.getLatLng())]);
+              }
+              $(attractionsfilter[categoryitem][itemID].marker._icon).addClass('active');
+            }, 100);
+
+            if (options.showPopup) {
+              var mlat = $(attractionsfilter[categoryitem][itemID].marker._latlng.lat);
+              var mlng = $(attractionsfilter[categoryitem][itemID].marker._latlng.lng);
+              mlat.push.apply(mlat, mlng);
+
+              map.panTo({
+                lat: mlat[0],
+                lng: mlat[1]
+              });
+              attractionsfilter[categoryitem][itemID].marker.openPopup();
+            }
           });
+
         });
-      } //
+      }
     }
+
   };
 
 })(jQuery);
