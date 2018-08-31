b6fa3a114f55bb0af085a8d4caceace7cfbc11b6
Kiran <kmekala@traveltripper.com>
2018-08-31 19:18:20 +0530

    Latest Instagram code  is implemented

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -37,35 +37,21 @@ $(document).ready(function() {
 $(window)
   .on("load", function (e) {
     var ownerId = '6142505566';
-    var instaurl = 'https://apinsta.herokuapp.com/u/stanfordcourtsf/';
+    var instaurl = 'https://igmedia.herokuapp.com/u/stanfordcourtsf/';
     var instaFeedUrl = "https://www.instagram.com/p/";
     $.ajax({
       url: instaurl,
       dataType: "json",
       success: function (response) {
 
-        console.log('ok insta', response);
         var showInstaFeeds = [],
           feedCount = 0;
-        var allFeeds = response.graphql.user.edge_owner_to_timeline_media.edges;
-        showInstaFeeds = $.grep(allFeeds, function (ele, i) {
-          return ele.node.owner.id == ownerId;
-        });
-        //console.log('testtt', showInstaFeeds);
-        if (showInstaFeeds.length < 6) {
-          for (var j = 0; j < allFeeds.length; j++) {
-            if (allFeeds[j].node.owner.id != ownerId) {
-              showInstaFeeds.push(allFeeds[j]);
-              feedCount++;
-              if (feedCount > 7)
-                break;
-            }
-          }
-        }
+
+
 
 
         setTimeout(function () {
-          $.each(showInstaFeeds, function (i, item) {
+          $.each(response.medias, function (i, item) {
             if ($(window)
               .width() >= 767) {
               if (i > 3) return false;
@@ -73,7 +59,7 @@ $(window)
               if (i > 3) return false;
             }
 
-            $('<div class="feed background-cover" style="background-image:url(' + item.node.thumbnail_src + ');"><a href="' + instaFeedUrl + item.node.shortcode + '" target="_blank"><i class="fab fa-instagram"></i><p class="insta-icons">@stanfordcourt <br><i class="far fa-heart" aria-hidden="true"></i>' + item.node.edge_liked_by.count + ' <i class="far fa-comment" aria-hidden="true"></i>' + item.node.edge_media_to_comment.count + '</p></a></div>')
+            $('<div class="feed background-cover" style="background-image:url(' + item.thumbnail + ');"><a href="' + instaFeedUrl + item.shortcode + '" target="_blank"><i class="fab fa-instagram"></i><p class="insta-icons">@stanfordcourt <br><i class="far fa-heart" aria-hidden="true"></i>' + item.like_count + ' <i class="far fa-comment" aria-hidden="true"></i>' + item.comment_count + '</p></a></div>')
               .appendTo('.instagram-feed');
           });
           var heightDIV = $('.instagram-feed div:first-child')
